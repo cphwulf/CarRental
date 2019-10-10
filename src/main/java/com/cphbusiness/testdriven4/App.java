b08de1fd1dbebd8859d33366990b3acc185354ed
Thor <thor@10.14.6>
2019-10-10 10:39:14 +0200

    next

--- src/main/java/com/cphbusiness/testdriven4/App.java
+++ src/main/java/com/cphbusiness/testdriven4/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "saxxxxaf World!" );
+        System.out.println( "xxxxxxaxxxxaf World!" );
     }
 }
