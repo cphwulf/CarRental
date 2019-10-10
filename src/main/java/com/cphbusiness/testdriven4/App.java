c2d12e21ff23dc6a8a49f334e5c228b873c30d30
Thor <thor@10.14.6>
2019-10-10 10:15:55 +0200

    first commit

--- src/main/java/com/cphbusiness/testdriven4/App.java
+++ src/main/java/com/cphbusiness/testdriven4/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helsaxxxxsdfaf World!" );
+        System.out.println( "Helsaxxxxaf World!" );
     }
 }
