8fda7e86c6028b90ebbd80c6fb321d1f021688e5
Thor <thor@10.14.6>
2019-10-10 10:11:14 +0200

    next

--- src/main/java/com/cphbusiness/testdriven4/App.java
+++ src/main/java/com/cphbusiness/testdriven4/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helsadf World!" );
+        System.out.println( "Helsasdfaf World!" );
     }
 }
