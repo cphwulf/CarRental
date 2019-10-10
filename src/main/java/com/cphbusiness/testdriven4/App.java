3b047ac5a6dedef3e70a969ab720a07eb365fc31
Thor <thor@10.14.6>
2019-10-10 09:45:35 +0200

    first commit

---
+++ src/main/java/com/cphbusiness/testdriven4/App.java
@@ -0,0 +1,13 @@
+package com.cphbusiness.testdriven4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/cphbusiness/testdriven4/AppTest.java
@@ -0,0 +1,19 @@
+package com.cphbusiness.testdriven4;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
